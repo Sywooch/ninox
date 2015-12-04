8ba3bb221c262dd1afad87689f6436f4b7c5bfa1
BoBRoID <n.gilko@gmail.com>
2015-12-04 16:34:37 +0200

    Restrict access to account for guests * Запретил доступ к модулю аккаунтов гостям

--- frontend/modules/account/controllers/DefaultController.php
+++ frontend/modules/account/controllers/DefaultController.php
@@ -6,6 +6,14 @@ use yii\web\Controller;
 
 class DefaultController extends Controller
 {
+    public function beforeAction($action){
+        if(\Yii::$app->user->isGuest){
+            \Yii::$app->response->redirect('/login');
+        }
+
+        return parent::beforeAction($action);
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
