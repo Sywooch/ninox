33c5538746001e319c01442c1073514b9eb1c5d6
Hellios3000 <Skeptic.q@gmail.com>
2015-12-04 16:21:37 +0200

    Added account module * Добавлен модуль "Аккаунт"

---
+++ frontend/modules/account/views/default/index.php
@@ -0,0 +1,21 @@
+<meta charset="UTF-8">
+<style>
+    .demo-icon
+    {
+        font-family: "shop";
+        font-style: normal;
+        font-weight: normal;
+        speak: none;
+    }
+</style>
+<link rel="stylesheet" href="css/animation.css"><!--[if IE 7]><link rel="stylesheet" href="css/shop-ie7.css"><![endif]-->
+<div class="content">
+    <div class="contentCenter">
+        <div id="icons" class="container">
+            <div title="Code: 0xe802" class="the-icon span3">
+                <i class="demo-icon icon-box">&#xe802;</i>
+
+            </div>
+        </div>
+    </div>
+</div>
