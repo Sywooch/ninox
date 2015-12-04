202c1425324b8f9374dcaa94e344f1769c554f6e
Hellios3000 <Skeptic.q@gmail.com>
2015-12-04 15:51:14 +0200

    added account page *not full ended

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -7,6 +7,8 @@ return [
         ''                  =>  'site/index',
         'tovar/<link>'      =>  'site/showtovar',
         '<action:(addtocart|removefromcart|order|getcart)>' =>  'site/<action>',
+        '<module>'          =>  '<module>/default/index',
+        '<module>/<actoin>'          =>  '<module>/default/<action>',
         '<url:(.*)>'        =>  'site/renderpage'
     ]
 ];
