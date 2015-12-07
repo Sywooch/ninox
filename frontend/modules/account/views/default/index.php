43359f670c5a5660c9ebb4d35881854787c93d6b
Hellios3000 <Skeptic.q@gmail.com>
2015-12-07 17:50:40 +0200

    working on the account module *Верстаю

--- frontend/modules/account/views/default/index.php
+++ frontend/modules/account/views/default/index.php
@@ -1,21 +1,142 @@
 <meta charset="UTF-8">
-<style>
-    .demo-icon
-    {
-        font-family: "shop";
-        font-style: normal;
-        font-weight: normal;
-        speak: none;
-    }
-</style>
-<link rel="stylesheet" href="css/animation.css"><!--[if IE 7]><link rel="stylesheet" href="css/shop-ie7.css"><![endif]-->
 <div class="content">
-    <div class="contentCenter">
-        <div id="icons" class="container">
-            <div title="Code: 0xe802" class="the-icon span3">
-                <i class="demo-icon icon-box">&#xe802;</i>
+    <div class="menu">
+        Менюшка
+    </div>
+    <div style="height: 445px">
+        <div class="page">
+            <div class="page">
+                <div class="user-account personal-data">
+                    <div class="myriad">
+                        <i class="icon icon-note"></i> Личные данные
+                    </div>
+                    <br>
+                    <div class="semi_font">
+                        <div>
+                            <div class="semi">
+                                Имя
+                            </div>
+                            <div class="personal-data">
+                                Nikola Zotov
+                            </div>
+                        </div>
+                        <div>
+                            <div class="semi">
+                                Телефон
+                            </div>
+                            <div  class="personal-data">
+                                <div class="personal-data">
+                                    +380932623049
+                                </div>
+                                <br>
+                                <div>
+                                    +380932521574
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="semi">
+                                Эл. почта
+                            </div>
+                            <div class="personal-data">
+                                info@gmail.com
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="page">
+                <div class="user-account">
+                    <div class="semi_font">
+                        <div class="color">
+                             <i class="icon icon-delivery-car"></i> Адрес для доставки:
+                        </div>
+                        <br>
+                        <div class="address">
+                            г. Киев, ул. Гетьмана Вадима, 1А/11
+                            <br>
+                            <br>
+                            Ивано-Франковск обл., г. Коломья Новая Почта 2 отд.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hr"></div>
+        <div class="user-options">
+            <a>Редактировать</a>
+            <br>
+            <a>Изменить Пароль</a>
+            <br>
+            <a>Выход</a>
+        </div>
+    </div>
+    <br>
+    <div>
+    <div class="user-account personal-data">
+        <div class="myriad">
+            <i class="icon icon-money-pig"></i> Личный счет
+        </div>
+        <div class="semi_font">
+            <div style="width: 450px">
+                <br>
+                <div class="semi">
+                    На вашем счету:
+                </div>
+                <br>
+                <div class="semi account-money">
+                    255
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="user-account personal-data">
+        <div class="myriad">
+            <i class="icon icon-your-discont"></i>  Ваша скидка
+        </div>
+        <div class="semi_font">
+            <div style="width: 450px">
+                <br>
+                <div class="address">
+                    Дисконтная карта - 2% на все товары на сайте
+                    <br>
+                    <br>
+                    Персональная скидка - 15% на <a>избранные</a> категории
+                </div>
 
             </div>
+        </div>
+    </div>
+    </div>
+    <div class="reviews user-account">
+        <div class="text">
+            <div class="myriad">
+               Сделайте наш сервис еще лучше
+            </div>
+            <div class="semi_font">
+                Оставьте отзывы о купленных вами товарах
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="my-orders">
+        <div class="user-account box">
+            <div class="myriad">
+                <i class="icon icon-box"></i> Мои заказы
+            </div>
+        </div>
+        <br>
+        <div class="order-waiting">
+            <i class="icon icon-arrow"></i>
+            dfd
+        </div>
+        <br>
+        <div class="order-complete">
+            <i class="icon icon-arrow"></i>
+        </div>
+        <br>
+        <div class="order-canceled">
+            <i class="icon icon-arrow"></i>
         </div>
     </div>
 </div>
