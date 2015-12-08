f4c5187593958e384a7a59e4268aff8b274dcc34
Hellios3000 <Skeptic.q@gmail.com>
2015-12-08 15:08:00 +0200

    working on the account module *Немного оптимизирован код

--- frontend/modules/account/views/default/index.php
+++ frontend/modules/account/views/default/index.php
@@ -3,140 +3,196 @@
     <div class="menu">
         Менюшка
     </div>
-    <div style="height: 445px">
-        <div class="page">
-            <div class="page">
-                <div class="user-account personal-data">
-                    <div class="myriad">
-                        <i class="icon icon-note"></i> Личные данные
-                    </div>
-                    <br>
-                    <div class="semi_font">
-                        <div>
-                            <div class="semi">
-                                Имя
-                            </div>
-                            <div class="personal-data">
-                                Nikola Zotov
-                            </div>
+    <div class="user-data-content">
+        <div class="user-account-data">
+            <div class="pages">
+                <div class="page">
+                    <div class="user-account personal-data">
+                        <div class="myriad">
+                            <i class="icon icon-note"></i> Личные данные
                         </div>
-                        <div>
-                            <div class="semi">
-                                Телефон
-                            </div>
-                            <div  class="personal-data">
+                        <div class="semi-font data">
+                            <div>
+                                <div class="semi">
+                                    Имя
+                                </div>
                                 <div class="personal-data">
-                                    +380932623049
-                                </div>
-                                <br>
-                                <div>
-                                    +380932521574
+                                    Nikola Zotov
                                 </div>
                             </div>
-                        </div>
-                        <div>
-                            <div class="semi">
-                                Эл. почта
+                            <div>
+                                <div class="semi">
+                                    Телефон
+                                </div>
+                                <div  class="personal-data">
+                                    <div class="personal-data">
+                                        +380932623049
+                                    </div>
+                                    <div>
+                                        +380932521574
+                                    </div>
+                                </div>
                             </div>
-                            <div class="personal-data">
-                                info@gmail.com
+                            <div>
+                                <div class="semi">
+                                    Эл. почта
+                                </div>
+                                <div class="personal-data">
+                                    info@gmail.com
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="page">
-                <div class="user-account">
-                    <div class="semi_font">
-                        <div class="color">
-                             <i class="icon icon-delivery-car"></i> Адрес для доставки:
-                        </div>
-                        <br>
-                        <div class="address">
-                            г. Киев, ул. Гетьмана Вадима, 1А/11
-                            <br>
-                            <br>
-                            Ивано-Франковск обл., г. Коломья Новая Почта 2 отд.
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="hr"></div>
-        <div class="user-options">
-            <a>Редактировать</a>
-            <br>
-            <a>Изменить Пароль</a>
-            <br>
-            <a>Выход</a>
-        </div>
-    </div>
-    <br>
-    <div>
-    <div class="user-account personal-data">
-        <div class="myriad">
-            <i class="icon icon-money-pig"></i> Личный счет
-        </div>
-        <div class="semi_font">
-            <div style="width: 450px">
-                <br>
-                <div class="semi">
-                    На вашем счету:
-                </div>
-                <br>
-                <div class="semi account-money">
-                    255
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="user-account personal-data">
-        <div class="myriad">
-            <i class="icon icon-your-discont"></i>  Ваша скидка
-        </div>
-        <div class="semi_font">
-            <div style="width: 450px">
-                <br>
-                <div class="address">
-                    Дисконтная карта - 2% на все товары на сайте
-                    <br>
-                    <br>
-                    Персональная скидка - 15% на <a>избранные</a> категории
-                </div>
+                <div class="page">
+                    <div class="user-account">
+                        <div class="semi-font">
+                            <div class="color">
+                                 <i class="icon icon-delivery-car"></i> Адрес для доставки:
+                            </div>
 
+                            <div class="address-data">
+                                <div class="address">
+                                    г. Киев, ул. Гетьмана Вадима, 1А/11
+                                </div>
+                                <div class="address">
+                                    Ивано-Франковск обл., г. Коломья Новая Почта 2 отд.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="user-options">
+                <div class="border">
+                    <a>Редактировать</a>
+                    <a>Изменить Пароль</a>
+                    <a>Выход</a>
+                </div>
             </div>
         </div>
-    </div>
-    </div>
-    <div class="reviews user-account">
-        <div class="text">
-            <div class="myriad">
-               Сделайте наш сервис еще лучше
+        <div class="user-money-discount">
+            <div class="user-account personal-discount">
+                <div class="myriad">
+                    <i class="icon icon-money-pig"></i> Личный счет
+                </div>
+                <div class="semi-font">
+                    <div class="address-data">
+                        <div class=" address">
+                            На вашем счету:
+                        </div>
+                            <div class="  account-money">
+                            255
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="semi_font">
-                Оставьте отзывы о купленных вами товарах
+            <div class="user-account personal-discount">
+                <div class="myriad">
+                    <i class="icon icon-your-discont"></i>  Ваша скидка
+                </div>
+                <div class="semi-font">
+                    <div class="address-data">
+                        <div class="address">
+                            Дисконтная карта - 2% на все товары на сайте
+                        </div>
+                        <div class="address">
+                            Персональная скидка - 15% на <a>избранные</a> категории
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <br>
-    <div class="my-orders">
-        <div class="user-account box">
-            <div class="myriad">
+        <div class="reviews user-account">
+            <div class="text">
+                <div class="myriad">
+                   Сделайте наш сервис еще лучше
+                </div>
+                <div class="semi-font">
+                    Оставьте отзывы о купленных вами товарах
+                </div>
+            </div>
+        </div>
+        <div class="my-orders">
+            <div class="user-account box myriad">
                 <i class="icon icon-box"></i> Мои заказы
             </div>
-        </div>
-        <br>
-        <div class="order-waiting">
-            <i class="icon icon-arrow"></i>
-            dfd
-        </div>
-        <br>
-        <div class="order-complete">
-            <i class="icon icon-arrow"></i>
-        </div>
-        <br>
-        <div class="order-canceled">
-            <i class="icon icon-arrow"></i>
+            <div class="orders">
+                <div class="order order-waiting">
+                    <i class="icon icon-arrow"></i>
+                    <div class="myriad">
+                        27227
+                    </div>
+                    <div class="data semi">
+                        01.09.2015
+                    </div>
+                    <div class="payment semi">
+                        Ожидается оплата
+                    </div>
+                    <div class="money semi">
+                        254 798 грн.
+                    </div>
+                    <div class="print semi">
+                        <i class="icon icon-print"></i>
+                    </div>
+                    <div class="history semi">
+                        <a>История</a>
+                    </div>
+                    <div class="reorder semi">
+                        <a>Повторить заказ</a>
+                    </div>
+                </div>
+                <div class="order order-complete">
+                    <i class="icon icon-arrow"></i>
+                    <div class="myriad">
+                        26587
+                    </div>
+                    <div class="data semi">
+                        15.08.2015
+                    </div>
+                    <div class="payment semi">
+                        Выполнен
+                    </div>
+                    <div class="money semi">
+                        1300 грн.
+                    </div>
+                    <div class="print semi">
+                        <i class="icon icon-print"></i>
+                    </div>
+                    <div class="history semi">
+                        <a>История</a>
+                    </div>
+                    <div class="reorder semi">
+                        <a>Повторить заказ</a>
+                    </div>
+                </div>
+                <div class="order order-canceled">
+                    <i class="icon icon-arrow"></i>
+                    <div class="myriad">
+                        26213
+                    </div>
+                    <div class="data semi">
+                        03.08.2015
+                    </div>
+                    <div class="payment semi">
+                        Отменен
+                    </div>
+                    <div class="money semi">
+                        254 586 798 грн.
+                    </div>
+                    <div class="print semi">
+                        <i class="icon icon-print"></i>
+                    </div>
+                    <div class="history semi">
+                        <a>История</a>
+                    </div>
+                    <div class="reorder semi">
+                        <a>Повторить заказ</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+
