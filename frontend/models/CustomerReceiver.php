8035430fe180325933d5777f134a854987318c61
BoBRoID <n.gilko@gmail.com>
2015-12-10 18:08:36 +0200

    Order create logic * Продолжение по логике оформления заказа

---
+++ frontend/models/CustomerReceiver.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bobroid
+ * Date: 10.12.15
+ * Time: 17:43
+ */
+
+namespace frontend\models;
+
+
+use common\models\CustomerAddresses;
+
+class CustomerReceiver extends CustomerAddresses{
+
+}
