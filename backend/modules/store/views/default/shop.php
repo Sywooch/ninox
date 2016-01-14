97f99d9437e48a6497c148a931205a38ef8202ea
BoBRoID <n.gilko@gmail.com>
2016-01-14 17:20:13 +0200

    some changes with cashbox

---
+++ backend/modules/store/views/default/shop.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bobroid
+ * Date: 14.01.16
+ * Time: 13:27
+ */
---
+++ console/migrations/m160114_114633_cashbox_shop.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160114_114633_cashbox_shop extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160114_114633_cashbox_shop cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
