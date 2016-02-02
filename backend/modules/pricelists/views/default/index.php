78f4a1ddc9b1aca0ac2896c89bf9100c609dfe15
BoBRoID <n.gilko@gmail.com>
2016-02-02 17:12:35 +0200

    update interface * Улучшения связаные с интерфейсом модуля

--- backend/modules/pricelists/views/default/index.php
+++ backend/modules/pricelists/views/default/index.php
@@ -1,33 +1,20 @@
 <?php
 use backend\modules\pricelists\models\PriceListForm;
+use rmrevin\yii\fontawesome\FA;
 
 $this->title = 'Прайсы';
 
-    $this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $this->title;
 
-    $modal = new \bobroid\remodal\Remodal([
-        'id'                    =>  'addPriceList',
-        'addRandomToID'         =>  false,
-        'confirmButtonOptions'  =>  [
-            'label' =>  'Сохранить',
-            'type'  =>  'submit',
-            'id'    =>  'submitForm'
-        ]
-    ]);
-
-$css = <<<'CSS'
-.remove{
-    -webkit-transition: all 0.2s ease-out;
-    -moz-transition: all 0.2s ease-out;
-    -o-transition: all 0.2s ease-out;
-    transition: all 0.2s ease-out;
-}
-
-.remove:hover{
-    cursor: pointer;
-    color: red;
-}
-CSS;
+$modal = new \bobroid\remodal\Remodal([
+    'id'                    =>  'addPriceList',
+    'addRandomToID'         =>  false,
+    'confirmButtonOptions'  =>  [
+        'label' =>  'Сохранить',
+        'type'  =>  'submit',
+        'id'    =>  'submitForm'
+    ]
+]);
 
 $js = <<<'JS'
 var removePrice = function(priceListID){
@@ -56,8 +43,6 @@ JS;
 
 $this->registerJs($js);
 
-$this->registerCss($css);
-
 $priceListForm = new PriceListForm();
 
 ?>
@@ -69,41 +54,58 @@ $priceListForm = new PriceListForm();
     'summary'       =>  false,
     'columns'       =>  [
         [
-            'format'    =>  'raw',
-            'width'     =>  '20px',
-            'value'     =>  function($model){
-                return \yii\bootstrap\Html::tag('span', \rmrevin\yii\fontawesome\FA::icon('times'), [
-                    'class' =>  'remove',
-                    'data-attribute-id' =>  $model->id
-                ]);
-            }
-        ],
-        [
             'attribute' =>  'name'
         ],
         [
             'attribute' =>  'format',
+            'width'     =>  '100px;',
             'value'     =>  function($model) use(&$priceListForm){
                 return $priceListForm->getFormats()[$model->format];
             }
         ],
         [
             'attribute' =>  'creator',
+            'format'    =>  'raw',
+            'width'     =>  '200px;',
             'value'     =>  function($model){
-                return $model->creator;
+                $creator = \common\models\Siteuser::getUser($model->creator);
+                return \yii\helpers\Html::tag('span', $creator->name, [
+                    'data-toggle'   =>  'tooltip',
+                    'title'         =>  $creator->username,
+                    'style'         =>'text-decoration: underline; cursor:pointer;'
+                ]);
             }
         ],
         [
             'attribute' =>  'categories',
+            'width'     =>  '160px;',
             'value'     =>  function($model){
                 return count($model->categories).' категорий';
             }
         ],
         [
-            'format'    =>  'html',
-            'value'     =>  function($model){
-                return \yii\helpers\Html::a('Посмотреть', \Yii::$app->params['frontend'].'/autopricelist/'.$model->id);
-            }
+            'class'     =>  \kartik\grid\ActionColumn::className(),
+            'buttons'   =>  [
+                'view'      =>  function($key, $model){
+                    return \yii\helpers\Html::a(FA::i('eye'), \Yii::$app->params['frontend'].'/autopricelist/'.$model->id, [
+                        'class' =>  'btn btn-sm btn-default',
+                        'target'    =>  '_blank'
+                    ]);
+                },
+                'update'      =>  function($key, $model){
+                    return \yii\helpers\Html::button(FA::i('edit'), [
+                        'class' =>  'btn btn-sm btn-default '
+                    ]);
+                },
+                'delete'    =>  function($key, $model){
+                    return \yii\helpers\Html::button(FA::i('trash'), [
+                        'class' =>  'btn btn-sm btn-danger remove',
+                        'data-attribute-id' =>  $model->id
+                    ]);
+                }
+            ],
+            'template'  =>  \yii\helpers\Html::tag('div', '{view}{update}{delete}', ['class' => 'btn-group btn-group-sm']),
+            'width'     =>  '120px;',
         ],
     ]
 ])?>
