c4cb21465f88e65481889699988beae7d471743c
BoBRoID <n.gilko@gmail.com>
2016-02-26 18:05:01 +0200

    Изменения в форме * Добавлены attributeLabels * Добавлены некоторые поля * Добавлены аддоны для текстовых полей

--- backend/modules/goods/models/GoodMainForm.php
+++ backend/modules/goods/models/GoodMainForm.php
@@ -17,36 +17,120 @@ class GoodMainForm extends Model{
     const ENABLED = 1;
     const DISABLED = 0;
 
+    /**
+     * @type integer ID товара
+     */
     public $id;
+
+    /**
+     * @type string название товара
+     */
     public $name;
+
+    /**
+     * @type string ссылка на изображение товара
+     */
     public $image;
+
+    /**
+     * @type string код товара
+     */
     public $code;
+
+    /**
+     * @type integer штрихкод товара
+     */
     public $barcode;
+
+    /**
+     * @type string артикул товара
+     */
+    public $additionalCode;
+
+    /**
+     * @type integer включен-ли товар
+     */
     public $enabled = self::DISABLED;
 
+    /**
+     * @type string мера измерения
+     */
     public $measure;
+
+    /**
+     * @type integer колличество в упаковке
+     */
     public $inPackageAmount;
+
+    /**
+     * @type bool известно-ли колличество в упаковке
+     */
     public $undefinedPackageAmount = false;
 
+    /**
+     * @type double оптовая цена
+     */
     public $wholesalePrice;
+
+    /**
+     * @type double розничная цена
+     */
     public $retailPrice;
+
 
     public $minQuantity;
     public $normalQuantity;
 
+    /**
+     * @type string описание товара
+     */
     public $description;
 
+    /**
+     * @type integer ID категории товара
+     */
     public $category;
 
+    /**
+     * @type double стоимость в валюте
+     */
     public $anotherCurrencyValue;
-    public $anotherCurrencyPeg;
+
+    /**
+     * @type bool пересчитывать товар по курсу автоматически
+     */
+    public $anotherCurrencyPeg = false;
+
+    /**
+     * @type string название валюты
+     */
     public $anotherCurrencyTag;
 
+    /**
+     * @type integer колличество для интернет-магазина
+     */
     public $count;
+
+    /**
+     * @type bool товар бесконечный
+     */
     public $isUnlimited = false;
+
+    /**
+     * @type bool товар "оригинал"
+     */
     public $isOriginal = false;
+
+    /**
+     * @type bool на товар есть гарантия
+     */
     public $haveGuarantee = false;
 
+    /**
+     * загружает в эту модель модель Good
+     *
+     * @param $good Good
+     */
     public function loadGood($good){
         foreach($this->modelAttributes() as $new => $old){
             $this->$new = $good->$old;
@@ -63,6 +147,7 @@ class GoodMainForm extends Model{
             'image'         =>  'ico',
             'code'          =>  'Code',
             'barcode'       =>  'BarCode1',
+            'additionalCode'=>  'BarCode2',
             'enabled'       =>  'show_img',
             'measure'       =>  'Measure1',
             'category'      =>  'GroupID',
@@ -72,6 +157,9 @@ class GoodMainForm extends Model{
         ];
     }
 
+    /**
+     * Сохраняет изменения в товар
+     */
     public function save(){
         $good = new Good();
 
@@ -83,5 +171,44 @@ class GoodMainForm extends Model{
         //$good->ico = $this->image;
         $good->Code = $this->code;
         $good->BarCode1 = $this->barcode;
+    }
+
+    /**
+     * Возвращает названия аттрибутов
+     *
+     * @return array
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id'            =>  'ID',
+            'name'          =>  'Название',
+            'description'   =>  'Описание',
+            'image'         =>  'Фото',
+            'code'          =>  'Код',
+            'barcode'       =>  'Штрихкод',
+            'additionalCode'=>  'Добавочный код',
+            'enabled'       =>  'Включен',
+            'measure'       =>  'Measure1',
+            'category'      =>  'Категория',
+            'inPackageAmount'=> 'штук в упаковке',
+            'wholesalePrice'=>  'Оптовая цена',
+            'retailPrice'   =>  'Розничная цена',
+            'anotherCurrencyTag'    =>  'Валюта',
+            'anotherCurrencyValue'  =>  'Цена в валюте',
+            'anotherCurrencyPeg'    =>  '',
+        ];
+    }
+
+    /**
+     * Возвращает валюты для редактирования товара
+     *
+     * @return array
+     */
+    public function getCurrencies(){
+        return [
+            'usd'   =>  'USD',
+            'eur'   =>  'EUR'
+        ];
     }
 }
--- backend/modules/goods/views/default/_goodEdit/main.php
+++ backend/modules/goods/views/default/_goodEdit/main.php
@@ -1,8 +1,26 @@
 <?php
 use common\models\Category;
+use kartik\form\ActiveField;
 use kartik\form\ActiveForm;
 use kartik\touchspin\TouchSpin;
+use rmrevin\yii\fontawesome\FA;
 use yii\helpers\Html;
+
+$css = <<<'CSS'
+.anotherCurrencyTagAddOn{
+    width: 72px;
+    padding: 0;
+}
+
+.anotherCurrencyTagAddOn select{
+    width: 100%;
+    height: 20px;
+    padding: 0;
+    margin: 0 10px;
+}
+CSS;
+
+$this->registerCss($css);
 
 $form = new ActiveForm([
     'id' => 'login-form-horizontal',
@@ -11,7 +29,12 @@ $form = new ActiveForm([
 ]);
 
 echo $form->field($model, 'name');
-echo $form->field($model, 'code');
+echo $form->field($model, 'code', [
+    'addon' => ['prepend' => ['content' => FA::i('key')]],
+    'hintType' => ActiveField::HINT_SPECIAL,
+    'hintSettings' => ['placement' => 'left', 'onLabelClick' => true, 'onLabelHover' => false]
+])
+    ->hint('Код является очень важной частью системы, поэтому редактировать его нежелательно!');
 echo $form->field($model, 'category')->widget(\kartik\select2\Select2::className(), [
     'data' => Category::getList(),
     'options' => [
@@ -21,7 +44,8 @@ echo $form->field($model, 'category')->widget(\kartik\select2\Select2::className
         'allowClear' => false
     ],
 ]);
-echo $form->field($model, 'barcode');
+echo $form->field($model, 'barcode', ['addon' => ['prepend' => ['content' => FA::i('barcode')]]]);
+echo $form->field($model, 'additionalCode', ['addon' => ['prepend' => ['content' => FA::i('tags')]]]);
 echo $form->field($model, 'description')->widget(\bobroid\imperavi\Widget::className(), [
     'model' => $model,
     'attribute' => 'description',
@@ -41,11 +65,12 @@ echo $form->field($model, 'description')->widget(\bobroid\imperavi\Widget::class
     ]
 ]);
 
-echo $form->field($model, 'barcode');
-echo $form->field($model, 'wholesalePrice');
-echo $form->field($model, 'retailPrice');
+echo $form->field($model, 'wholesalePrice', ['addon' => ['prepend' => ['content' => '₴']]]);
+echo $form->field($model, 'retailPrice', ['addon' => ['prepend' => ['content' => '₴']]]);
 echo $form->field($model, 'inPackageAmount', [
-    'template'      =>  '{label}<div style="margin-left: -15px;" class="col-xs-8">{input}'.$form->field($model, 'undefinedPackageAmount', ['options' => ['class' => 'col-xs-3']])->checkbox(['label' => 'неизвестно']).'</div>',
+    'template'      =>  '{label}'.Html::tag('div', '{input}'.$form
+                ->field($model, 'undefinedPackageAmount', ['options' => ['class' => 'col-xs-3']])
+                ->checkbox(['label' => 'неизвестно']), ['style' => 'margin-left: -15px;', 'class' => 'col-xs-8']),
     'inputOptions'  =>  [
         'class' =>  'col-xs-6'
     ]
@@ -57,3 +82,23 @@ echo $form->field($model, 'inPackageAmount', [
         'max'   =>  10000
     ]
 ]);
+echo $form->field($model, 'anotherCurrencyValue', [
+    'addon' => [
+        'prepend'   => [
+            'content' => $form
+                ->field($model, 'anotherCurrencyPeg', ['template' => '{input}'])->checkbox([], false)->label(false)
+        ],
+        'append'    =>  [
+            'content'   =>  $form
+                ->field($model, 'anotherCurrencyTag', ['template' => '{input}'])
+                ->label(false)
+                ->dropDownList($model->getCurrencies()),
+            'options'   =>  [
+                'class' =>  'anotherCurrencyTagAddOn'
+            ]
+        ]
+    ]
+]);
+//echo $form->field($model, '');
+
+echo Html::tag('div', '', ['class' => 'clearfix']);
