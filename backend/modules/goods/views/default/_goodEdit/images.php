a6472c0eca58f9f8023de022b6a80af8993edec9
BoBRoID <n.gilko@gmail.com>
2016-03-01 16:03:17 +0200

    some fixes

--- backend/modules/goods/views/default/_goodEdit/images.php
+++ backend/modules/goods/views/default/_goodEdit/images.php
@@ -1,11 +1,35 @@
 <?php
 use kartik\file\FileInput;
+use rmrevin\yii\fontawesome\FA;
+use yii\bootstrap\Html;
 use yii\helpers\Url;
 
 $css = <<<'CSS'
-.file-preview-image{
-    max-width: 100px;
+.file-preview{
+    border: none;
+    border-radius: 0;
+    padding: 0;
+    margin: 0;
+}
+
+.file-preview .file-drop-zone{
+    border: none;
+    border-radius: 0;
+    margin: 0;
+    padding: 0;
+}
+
+.file-preview .file-footer-caption{
+    width: 152px;
+}
+
+.file-preview .file-preview-image{
     max-height: 100px;
+}
+
+.file-preview .file-drop-zone.file-highlighted {
+    border: 0 !important;
+    outline: 2px dashed #999 !important;
 }
 CSS;
 
@@ -24,6 +48,9 @@ foreach($good->photos as $photo){
     $photos[] = [
         'caption'   =>  $photo->ico,
         'key'       =>  $photo->id, //TODO: Заменить на другое значение ключа
+        'extra'     =>  [
+            'order' =>  $photo->order
+        ],
         'frameAttr' =>  [
             'style'     =>  'max-height: 80px; max-width: 80px;'
         ]
@@ -41,7 +68,22 @@ echo FileInput::widget([
     'pluginOptions' => [
         'initialPreview'        =>  $previewPhotos,
         'initialPreviewConfig'  =>  $photos,
-        'previewFileType'       =>  'any',
+        'overwriteInitial'      =>  false,
+        'uploadExtraData'       =>  [
+            'goodID'            =>  $good->ID
+        ],
+        'layoutTemplates'       =>  [
+            'actions'           =>  '<div class="file-actions">
+<div class="file-footer-buttons">{upload}{delete}'.Html::button(FA::i('star'), ['class' => 'btn btn-default btn-xs', 'style' => 'color: #ffcc00']).'</div>
+<div class="file-upload-indicator" tabindex="-1" title="{indicatorTitle}">{indicator}</div>
+<div class="clearfix"></div>
+</div>',
+        ],
+        'showCaption'           =>  false,
+        'showUpload'            =>  false,
+        'showRemove'            =>  false,
+        'showClose'             =>  false,
+        'previewFileType'       =>  'image',
         'deleteUrl'             =>  Url::toRoute(['/goods/photo', 'act' => 'delete']),
         'uploadUrl'             =>  Url::toRoute(['/goods/photo', 'act' => 'upload']),
     ]
