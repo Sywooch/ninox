cab3e124673552810be0bbf6bad178cf782326d7
Hellios3000 <Skeptic.q@gmail.com>
2016-03-15 16:23:42 +0200

    Main * git fix

---
+++ frontend/web/js/mainMenu.js
@@ -0,0 +1,36 @@
+$('img.svg').each(function(){
+    var img = jQuery(this),
+        imgID = img.attr('id'),
+        imgClass = img.attr('class'),
+        imgURL = img.attr('src');
+
+    $.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var svg = jQuery(data).find('svg'),
+            width = '50',
+            height = '50';
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            svg = svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            svg = svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        svg = svg.removeAttr('xmlns:a');
+
+        svg.attr('width', width);
+        svg.attr('height', height);
+
+        svg.attr('preserveAspectRatio', 'xMinYMin meet');//preserveAspectRatio="xMinYMin meet"
+        svg.attr('viewBox', '0 0 ' + (width * 20) + ' ' + (height * 20));
+
+
+        // Replace image with new SVG
+        img.replaceWith(svg);
+
+    }, 'xml');
+});
