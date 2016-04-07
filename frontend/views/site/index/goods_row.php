15f0470a6fa39cfd452e9bc1ec851da7a76f7fa1
BoBRoID <n.gilko@gmail.com>
2016-04-07 14:25:07 +0300

    hotfix

--- frontend/views/site/index/goods_row.php
+++ frontend/views/site/index/goods_row.php
@@ -1,4 +1,7 @@
-<?php use kop\y2sp\ScrollPager; ?>
+<?php use kop\y2sp\ScrollPager;
+
+\rmrevin\yii\fontawesome\cdn\AssetBundle::register($this);
+use yii\bootstrap\Html; ?>
 <div class="goods-content-all">
     <?=\yii\widgets\ListView::widget([
         'dataProvider'	=>	$dataProvider,
@@ -21,11 +24,13 @@
                 //ScrollPager::EXTENSION_NONE_LEFT,
                 ScrollPager::EXTENSION_PAGING,
             ],
+            'triggerTemplate'   =>  Html::tag('div', Html::tag('span', \Yii::t('shop', 'СМОТРЕТЬ ВСЕ ТОВАРЫ')), ['class' => 'goods-item goods-item-style']),
+            'spinnerTemplate'   =>  Html::tag('div', Html::tag('span', \rmrevin\yii\fontawesome\FA::i('refresh')->addCssClass('fa-spin')), ['class' => 'goods-item-style', 'style' => 'height: 345px; width: 215px; float: left; margin-left: 10px; margin-bottom: 20px; margin-right: 10px;']),
             'delay'    =>  0
         ],
         'summary'	=>	false
     ])?>
     <!--<div class="goods-item goods-item-style">
-        <span><?=\Yii::t('shop', 'СМОТРЕТЬ ВСЕ ТОВАРЫ')?></span>
+        <span>></span>
     </div>-->
 </div>
