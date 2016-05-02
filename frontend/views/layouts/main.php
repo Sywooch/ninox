218e38a39215ef71a54d2632033e50748881f758
bobroid <n.gilko@gmail.com>
2016-05-02 14:10:27 +0300

    Перевёрстка и перевод нижней части сайта

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -341,7 +341,7 @@ $this->beginPage();
 										<div>
 											<div class="blue-white-phone"></div>
 											<span class="semibold">0 800 508 208</span>
-											<span class="free-call">бесплатно со стационарных</span>
+											<span class="free-call"></span>
 										</div>
 										<div class="city-number">
 											<span class="city">Киев</span>
@@ -449,107 +449,136 @@ $this->beginPage();
 					?>
 				</div>
 			</div>
-			<div class="footer-menu">
-				<div class="goods-item">
-					<?=Html::tag('span', \Yii::t('shop', 'О компании'), [
+			<?=Html::tag('div',
+				Html::tag('div',
+					Html::tag('span', \Yii::t('shop', 'О компании'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/o-nas'
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Контакты'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/kontakty'
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Вакансии'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/vakansii'
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Блог'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/blog'
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Отзывы о магазине'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/otzyvy'
-					]),
+					]).
 					Html::tag('a', \Yii::t('shop', 'Карта сайта'), [
 						'class'		=>	'link-hide',
 						'href'		=>	'/map'
-					])?>
-				</div>
-				<div class="goods-item">
-					<?=Html::tag('span', \Yii::t('shop', 'Услуги'), [
+					]),
+					[
+						'class'	=>	'goods-item'
+					]).
+				Html::tag('div',
+					Html::tag('span', \Yii::t('shop', 'Услуги'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/status-zakaza'//хз
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Акции'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/akcii'
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Гарантии'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/garantii'
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Бонусная программа'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/kontakty'//хз
-					])
-					?>
-				</div>
-				<div class="goods-item">
-					<?=Html::tag('span', \Yii::t('shop', 'Как заказать'), [
+					]),
+					[
+						'class'	=>	'goods-item'
+					]).
+				Html::tag('div',
+					Html::tag('span', \Yii::t('shop', 'Как заказать'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/pomoshch'
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Оплата'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/oplata'
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Доставка'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/dostavka'
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Возврат товара'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/vozvrat-i-obmen'
-					])
-					?>
-				</div>
-				<div class="goods-item feedback-link">
-					<?=Html::tag('span', \Yii::t('shop', 'Обратная связь'), [
+					]),
+					[
+						'class'	=>	'goods-item'
+					]).
+				Html::tag('div',
+					Html::tag('span', \Yii::t('shop', 'Обратная связь'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/vakansii'//хз
-					]),
+					]).
 					Html::tag('span', \Yii::t('shop', 'Проблемы с заказом?'), [
 						'class'		=>	'link-hide',
 						'data-href'	=>	'/kontakty'//хз
-					])
-					?>
-				</div>
-			</div>
-			<div class="feedback-block">
-				<div class="footer-content">
-					<div class="card">
-						<img src="/img/site/visa-icon.png">
-						<img src="/img/site/mastercard-icon.png">
-						<img src="/img/site/privat24-icon.png">
-					</div>
-					<div class="socialNetworks">
-						<?=SocialButtonWidget::widget([
-														  'items' => [
-															  ['linkTag' => 'a', 'link' => 'https://www.facebook.com/krasota.style.com.ua', 'type' => 'facebook'],
-															  ['linkTag' => 'a', 'link' => 'http://vk.com/bizhuteria_optom_ua', 'type' => 'vkontakte'],
-															  ['linkTag' => 'a', 'link' => 'https://plus.google.com/u/0/106125731561025796307?rel=author', 'type' => 'googleplus'],
-															  ['linkTag' => 'a', 'link' => 'http://www.odnoklassniki.ru/krasotastyle2', 'type' => 'odnoklassniki'],
-															  ['linkTag' => 'a', 'link' => 'https://twitter.com/krasota_style', 'type' => 'twitter'],
-														  ]
-													  ])?>
-					</div>
-				</div>
-			</div>
-			<div class="footer-content">
-				<span class="left">© Интернет-магазин «krasota-style™» 2011–2015</span>
-				<span class="right">Дизайн и разработка сайта “krasota-style.ua”</span>
-			</div>
+					]),
+					[
+						'class'	=>	'goods-item feedback-link'
+					]),
+				[
+					'class'	=>	'footer-menu'
+				]).
+			Html::tag('div',
+				Html::tag('div',
+					Html::tag('div',
+						Html::img('/img/site/visa-icon.png').
+						Html::img('/img/site/mastercard-icon.png').
+						Html::img('/img/site/privat24-icon.png'),
+						[
+							'class'	=>	'card'
+						]).
+					Html::tag('div',
+						SocialButtonWidget::widget([
+							'items' => [
+								['linkTag' => 'a', 'link' => 'https://www.facebook.com/krasota.style.com.ua', 'type' => 'facebook'],
+								['linkTag' => 'a', 'link' => 'http://vk.com/bizhuteria_optom_ua', 'type' => 'vkontakte'],
+								['linkTag' => 'a', 'link' => 'https://plus.google.com/u/0/106125731561025796307?rel=author', 'type' => 'googleplus'],
+								['linkTag' => 'a', 'link' => 'http://www.odnoklassniki.ru/krasotastyle2', 'type' => 'odnoklassniki'],
+								['linkTag' => 'a', 'link' => 'https://twitter.com/krasota_style', 'type' => 'twitter'],
+							]
+						]),
+						[
+							'class'	=>	'socialNetworks'
+						]),
+					[
+						'class'	=>	'footer-content'
+					]),
+				[
+					'class'	=>	'feedback-block'
+				]).
+			Html::tag('div',
+				Html::tag('span',
+					\Yii::t('shop', '© Интернет-магазин «krasota-style™» 2011–{year}', ['year' => date('Y')]),
+					[
+						'class' => 'left'
+					]
+				).'&nbsp;'.
+				Html::tag('span',
+					\Yii::t('shop', 'Дизайн и разработка сайта “krasota-style.ua”',
+						[
+							'class' => 'right'
+						]
+					)
+				),
+				[
+					'class'	=>	'footer-content'
+				]
+			)?>
 		</div>
 		<?=$cartModal->renderModal(),
 		$loginModal->renderModal(),
