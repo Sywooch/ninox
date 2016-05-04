8ff729e01a25b4207fc3154ea3cad01961ddc9c5
bobroid <n.gilko@gmail.com>
2016-05-04 19:37:03 +0300

    fix some column

--- console/migrations/m160504_163413_fix_nakladna.php
+++ console/migrations/m160504_163413_fix_nakladna.php
@@ -7,6 +7,7 @@ class m160504_163413_fix_nakladna extends Migration
     public function up()
     {
         $this->alterColumn('history', 'nakladna', \yii\db\Schema::TYPE_STRING."(32) DEFAULT NULL");
+        $this->alterColumn('history', 'moneyConfirmedDate', \yii\db\Schema::TYPE_DATETIME." DEFAULT NULL");
     }
 
     public function down()
