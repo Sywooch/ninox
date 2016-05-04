823bbfb2de7ca60e2103af76e21b71c6117a69df
Hellios3000 <Skeptic.q@gmail.com>
2016-05-04 19:05:59 +0300

    reset password

--- frontend/views/site/resetPassword.php
+++ frontend/views/site/resetPassword.php
@@ -7,30 +7,32 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = \Yii::t('shop', 'Reset password');
+$this->title = \Yii::t('shop', 'Востановление пароля');
 $this->params['breadcrumbs'][] = $this->title;
 
 $this->registerMetaTag(['name' => 'robots', 'content' => 'noindex, follow'], 'robots');
 ?>
-<div class="site-reset-password">
-    <?=
-        Html::tag('h1', Html::encode($this->title)).
-        Html::tag('p', \Yii::t('shop', 'Please choose your new password:'))
-    ?>
+<div class="content">
+    <div class="site-reset-password">
+        <?=
+            Html::tag('h1', Html::encode($this->title)).
+            Html::tag('p', \Yii::t('shop', 'Пожалуйста введите Ваш новый пароль:'))
+        ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']);
+        <div class="row">
+            <div class="col-lg-5">
+                <?php $form = ActiveForm::begin(['id' => 'reset-password-form']);
 
-                echo $form->field($model, 'password')->passwordInput(),
-                    Html::tag('div',
-                        Html::submitButton(\Yii::t('shop', 'сохранить'), ['class' => 'btn btn-primary']),
-                        [
-                            'class' => 'form-group'
-                        ]
-                    );
+                    echo $form->field($model, 'пароль')->passwordInput(),
+                        Html::tag('div',
+                            Html::submitButton(\Yii::t('shop', 'сохранить'), ['class' => 'btn btn-primary']),
+                            [
+                                'class' => 'form-group'
+                            ]
+                        );
 
-             ActiveForm::end(); ?>
+                 ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
