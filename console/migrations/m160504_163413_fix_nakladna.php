260ae2a801a535de9fe81362cd53a3564f4c6f0c
bobroid <n.gilko@gmail.com>
2016-05-04 19:35:33 +0300

    fix some column

---
+++ console/migrations/m160504_163413_fix_nakladna.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160504_163413_fix_nakladna extends Migration
+{
+    public function up()
+    {
+        $this->alterColumn('history', 'nakladna', \yii\db\Schema::TYPE_STRING."(32) DEFAULT NULL");
+    }
+
+    public function down()
+    {
+        echo "m160504_163413_fix_nakladna cannot be reverted.\n";
+
+        return true;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
