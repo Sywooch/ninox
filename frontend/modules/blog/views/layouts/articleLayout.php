65e1278d86ff62749a36c5b1d015f674add8cf40
bobroid <n.gilko@gmail.com>
2016-05-06 16:02:42 +0300

    module nav fix

--- frontend/modules/blog/views/layouts/articleLayout.php
+++ frontend/modules/blog/views/layouts/articleLayout.php
@@ -38,39 +38,6 @@ $this->beginPage() ?>
 <?php $this->beginBody() ?>
 
 <div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'Krasota-Style blog',
-        'brandUrl' => (Yii::$app->params['frontend'].'/blog'),
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index'], 'items' => [['label' => 'About', 'url' => ['/site/about']]]],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
-
     <div class="container">
         <?= \frontend\modules\blog\widgets\MenuWidget::widget([
             'items' =>  \common\models\BlogCategory::find()->with('childs')->where(['parent' => 0, 'taxonomy' => 'category'])->all()
