20e89c02d0cff16fcf5dee8a37d633fc862f7ad4
Hellios3000 <Skeptic.q@gmail.com>
2016-05-11 18:10:47 +0300

    scroll o-nas * доделать переход с контактов и помощи в о-нас по якорю

--- frontend/views/site/_left_menu.php
+++ frontend/views/site/_left_menu.php
@@ -9,14 +9,55 @@ use yii\bootstrap\Html;
 
 $js = <<<JS
 
+var lastId,
+    topMenu = $(".menu-link"),
+    topMenuHeight = topMenu.outerHeight()-50,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
 $(document).ready(function(){
     $(window).scroll(function() {
         var top = $(document).scrollTop();
         if (top < 100) $(".left-side").css({top: '0', position: 'relative'});
         else $(".left-side").css({top: '130px', position: 'fixed'});
+    // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .removeClass("active")
+         .filter("[href='#"+id+"']").addClass("active");
+   }
     });
 });
-
+/*
 function scrollToAnchor(aid){
     $('html,body').animate({scrollTop: ($("a[name='"+ aid +"']").offset().top - 120)},2000);
 }
@@ -28,47 +69,17 @@ $("body").on('click', '.left-menu-links .menu-link .list-group-item', function(e
 
 $(document).ready(function(){
     scrollToAnchor('about-work-header');
-});
-/*
+});*/
 
-var menu_selector = ".list-group"; // Переменная должна содержать название класса или идентификатора, обертки нашего меню.
+// Cache selectors
 
-function onScroll(){
-    var scroll_top = $(document).scrollTop();
-    $(menu_selector + " a").each(function(){
-        var hash = $(this).attr("href");
-        var target = $(hash);
-        if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
-            $(menu_selector + " a.active").removeClass("active");
-            $(this).addClass("active");
-        } else {
-            $(this).removeClass("active");
-        }
+
+/*$(function(){
+    $(window).scroll(function() {
+        var top = $(document).scrollTop();
+        if (top < 100) $(".left-side").css({top: '0', position: 'relative'});
+        else $(".left-side").css({top: '130px', position: 'fixed'});
     });
-}
-
-$(document).ready(function () {
-
-    $(document).on("scroll", onScroll);
-
-    $("a[href^=#]").click(function(e){
-        e.preventDefault();
-
-        $(document).off("scroll");
-        $(menu_selector + " a.active").removeClass("active");
-        $(this).addClass("active");
-        var hash = $(this).prop("href").replace(/(.*)\#/, '');
-        var target = $(hash);
-
-        $("html, body").animate({
-            scrollTop: target.offset().top
-        }, 500, function(){
-            window.location.hash = hash;
-            $(document).on("scroll", onScroll);
-        });
-
-    });
-
 });*/
 
 JS;
@@ -79,20 +90,20 @@ $this->registerJs($js);
         \frontend\widgets\ListGroupMenu::widget([
             'items'    => [
                 [
-                    'label' =>  \Yii::t('shop', 'Как мы работаем',[ 'class' =>  'menu']),
-                    'href'  =>  '/o-nas#about-work-header'
+                    'label' =>  \Yii::t('shop', 'Как мы работаем'),
+                    'href'  =>  '#about-work-header'
                 ],
                 [
                     'label' =>  \Yii::t('shop', 'Доставка и оплата'),
-                    'href'  =>  '/o-nas#about-delivery-payment-header'
+                    'href'  =>  '#about-delivery-payment-header'
                 ],
                 [
                     'label' =>  \Yii::t('shop', 'Гарантии и возврат'),
-                    'href'  =>  '/o-nas#about-return-header'
+                    'href'  =>  '#about-return-header'
                 ],
                 [
                     'label' =>  \Yii::t('shop', 'Условия исп. сайта'),
-                    'href'  =>  '/o-nas#about-TermOfUse-header'
+                    'href'  =>  '#about-TermOfUse-header'
                 ],
             ]
         ]),
