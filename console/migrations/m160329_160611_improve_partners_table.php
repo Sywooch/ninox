ef60f68d57fbecdd04a385cb2145207cb0ef93b7
panchenkodv <lexx.klan@mail.ru>
2016-05-12 12:59:52 +0300

    partnerDeliveryType fix

--- console/migrations/m160329_160611_improve_partners_table.php
+++ console/migrations/m160329_160611_improve_partners_table.php
@@ -11,11 +11,6 @@ class m160329_160611_improve_partners_table extends Migration
         $this->addColumn('partners', 'deliveryInfo', Schema::TYPE_STRING);
         $this->addColumn('partners', 'paymentParam', Schema::TYPE_INTEGER.' UNSIGNED DEFAULT NULL');
         $this->addColumn('partners', 'paymentInfo', Schema::TYPE_STRING);
-
-        $this->renameColumn('partners', 'PaymentType', 'paymentType');
-        $this->renameColumn('partners', 'shippingType', 'deliveryType');
-        $this->alterColumn('partners', 'paymentType', Schema::TYPE_INTEGER.' UNSIGNED DEFAULT NULL');
-        $this->alterColumn('partners', 'deliveryType', Schema::TYPE_INTEGER.' UNSIGNED DEFAULT NULL');
     }
 
 
