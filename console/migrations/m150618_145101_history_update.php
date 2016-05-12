f6e6ba38a48bb9dbbb6d356ec2926cfce5bfe715
panchenkodv <lexx.klan@mail.ru>
2016-05-12 09:46:18 +0300

    fix data trancate

--- console/migrations/m150618_145101_history_update.php
+++ console/migrations/m150618_145101_history_update.php
@@ -1,6 +1,5 @@
 <?php
 
-use yii\db\Schema;
 use yii\db\Migration;
 
 class m150618_145101_history_update extends Migration
@@ -8,6 +7,7 @@ class m150618_145101_history_update extends Migration
     public function up()
     {
         $this->execute("UPDATE `history` SET `dostavka` = '2' WHERE `dostavka` = 'Новая почта' OR `dostavka` = '';
+UPDATE `history` SET `dostavka` = REPLACE(`dostavka`, ',', '') WHERE `dostavka` LIKE '%,%';
 UPDATE `history` SET `plateg` = '1' WHERE `plateg` = 'наложенный платеж' OR `plateg` = 'наложенным платежом' OR `plateg` = 'РЅР°Р»РѕР¶РµРЅРЅС‹Рј РїР»Р°С‚РµР¶РѕРј';
 UPDATE `history` SET `plateg` = '2' WHERE `plateg` = 'на банковскую карту' OR `plateg` = 'РЅР° Р±Р°РЅРєРѕРІСЃРєСѓСЋ РєР°СЂС‚Сѓ';
 UPDATE `history` SET `plateg` = '0' WHERE `plateg` = '' OR `plateg` = 'способ платежа не выбран';
@@ -22,7 +22,7 @@ ALTER TABLE `history` DROP COLUMN `callState`;");
             CHANGE `adress` `deliveryAddress` VARCHAR(255),
             CHANGE `oblast` `deliveryRegion` VARCHAR(255),
             CHANGE `displayorder` `added` INT(11),
-            CHANGE `dostavka` `deliveryType` INT(2) NOT NULL DEFAULT 0,
+            CHANGE `dostavka` `deliveryType` INT(4) NOT NULL DEFAULT 0,
             CHANGE `city` `deliveryCity` VARCHAR(255),
             CHANGE `text` `customerComment` TEXT,
             CHANGE `userid` `customerID` INT(11) NOT NULL DEFAULT 0,
@@ -30,9 +30,9 @@ ALTER TABLE `history` DROP COLUMN `callState`;");
             CHANGE `kupon` `coupon` VARCHAR(255),
             CHANGE `plateg` `paymentType` INT(11) NOT NULL DEFAULT 0,
             CHANGE `plateg_privat` `paymentInfo` VARCHAR(255),
-            CHANGE `perezvon` `callback` INT(2) NOT NULL DEFAULT 0,
+            CHANGE `perezvon` `callback` INT(4) NOT NULL DEFAULT 0,
             CHANGE `zamena` `canChangeItems` INT(2) NOT NULL DEFAULT 0,
-            CHANGE `fakt_summ` `actualAmount` INT(2) NOT NULL DEFAULT 0,
+            CHANGE `fakt_summ` `actualAmount` DOUBLE(24, 2) NOT NULL DEFAULT 0,
             CHANGE `sendnakladna` `nakladnaSendState` INT(2) NOT NULL DEFAULT 0,
             CHANGE `done_userid` `responsibleUserID` INT(11) NOT NULL DEFAULT 0,
             CHANGE `confirm` `confirmed` INT(1) NOT NULL DEFAULT 0,
@@ -41,8 +41,8 @@ ALTER TABLE `history` DROP COLUMN `callState`;");
             CHANGE `date_done` `doneDate` DATETIME,
             CHANGE `date_send` `sendDate` DATETIME,
             CHANGE `date_recieved_np` `receivedDate` DATETIME,
-            CHANGE `sendsms` `smsState` INT(1) NOT NULL DEFAULT 0,
-            CHANGE `trash` `deleted` INT(1) NOT NULL DEFAULT 0,
+            CHANGE `sendsms` `smsState` INT(4) NOT NULL DEFAULT 0,
+            CHANGE `trash` `deleted` INT(4) NOT NULL DEFAULT 0,
 
             ADD (
                 `confirmedDate` DATETIME,
