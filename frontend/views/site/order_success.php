a86783d1100d73a3d47b302d7d113fb8a11e9eea
panchenkodv <lexx.klan@mail.ru>
2016-05-19 18:42:48 +0300

    eCommerce + дефолтные оплата и дсставка (не совсем удачные) + фикс таблицы

--- frontend/views/site/order_success.php
+++ frontend/views/site/order_success.php
@@ -1,3 +1,55 @@
 <?php
 
+use backend\models\History;
+use yii\web\View;
+
+$order = History::find()->where(['ID' => $model->createdOrder])->one();
+
+$products = [];
+
+foreach($order->items as $item){
+	$products[] = [
+		'sku' => $item->good->ID,
+		'name' => $item->good->Name,
+		'category' => $item->good->category->name,
+		'price' => $item->originalPrice,
+		'quantity' => $item->originalCount
+	];
+}
+
+$params = [
+	'transactionId' => $order->number,
+	'transactionAffiliation' => 'KrasotaStyle',
+	'transactionTotal' => $order->orderSum,
+	'transactionTax' => 0,
+	'transactionShipping' => 0,
+	'transactionProducts' => $products,
+	'event' => 'trackTrans'
+];
+
+$eCommerce = 'dataLayer = [{';
+
+foreach($params as $key => $param){
+	switch($key){
+		case 'transactionProducts':
+			$eCommerce .= "'{$key}': [";
+			foreach($param as $item){
+				$eCommerce .= "{";
+				foreach($item as $ikey => $iparam){
+					$eCommerce .= "'{$ikey}': '{$iparam}',";
+				}
+				$eCommerce .= "},";
+			}
+			$eCommerce .= "],";
+			break;
+		default:
+			$eCommerce .= "'{$key}': '{$param}',";
+			break;
+	}
+}
+
+$eCommerce .= '}];';
+
+$this->registerJs($eCommerce, View::POS_BEGIN);
+
 echo \Yii::t('shop', 'Ваш заказ оформлен! Спасибо!');
