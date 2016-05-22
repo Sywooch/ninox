abb03666bf97665512ce8c8fcaf3213c4a700503
Hellios3000 <Skeptic.q@gmail.com>
2016-05-22 16:13:07 +0300

    Страница заказов

---
+++ backend/modules/orders/views/default/_review_accordion.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hellios
+ * Date: 22.05.16
+ * Time: 15:36
+ */
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+?>
+
+<div class="col-xs-10" style="border: 1px solid #eceef2;">Все получили, абсолютно всем довольны ( качеством
+    обслуживания , качеством товара) - успели к дню Святого Николая сделать своим детям сюрпризы. Большое спасибо, удачи в работе и с праздниками Вас !!!</div>
+<div class="col-xs-2">
+<?php
+echo Html::a('Сохранить', '', [
+    'class' =>  'btn btn-default'
+])
+?>
+    </div>
+
