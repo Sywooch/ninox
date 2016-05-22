8787ce8b8f190a5e5e4472a66213b6e1904bade0
bobroid <n.gilko@gmail.com>
2016-05-22 16:28:54 +0300

    Коммит в ветку order

---
+++ backend/modules/orders/views/default/_order_history.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hellios
+ * Date: 22.05.16
+ * Time: 15:53
+ */
+?>
+<div class="col-xs-12 col-sm-6 col-md-8 block-span" style="text-align: left">
+    <b>История заказа</b>
+    <div class="blue-line"></div>
+    <span><b>sdfgd</b>dfgdfg</span>
+    <span><b>sdfgd</b>dfgdfg</span>
+    <span><b>sdfgd</b>dfgdfg</span>
+    <span><b>sdfgd</b>dfgdfg</span>
+    <span><b>sdfgd</b>dfgdfg</span>
+    <span><b>sdfgd</b>dfgdfg</span>
+    <span><b>sdfgd</b>dfgdfg</span>
+    <div class="blue-line"></div>
+    <span><b>sdfgd</b>dfgdfg</span>
+
+
+</div>
+<div class="col-xs-6 col-md-4" style="text-align: left">
+    <b>Статус смс</b>
+    <div class="blue-line"></div>
+    <ul>
+        <li><span>Не дозвонились</span></li>
+        <li><span>Не дозвонились</span></li>
+        <li><span>Не дозвонились</span></li>
+        <li><span>Не дозвонились</span></li>
+    </ul>
+
+</div>
