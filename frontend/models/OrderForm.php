c8352189decae794a1c4a4a94a271761b6c713fa
panchenkodv <lexx.klan@mail.ru>
2016-05-23 15:12:24 +0300

    фикс сброс ценовіх правил в корзине при оформлении заказа

--- frontend/models/OrderForm.php
+++ frontend/models/OrderForm.php
@@ -300,7 +300,7 @@ class OrderForm extends Model{
         if($order->save()){
             $this->createdOrder = $order->id;
 
-            foreach(\Yii::$app->cart->goods as $good){
+            foreach(\Yii::$app->cart->goods as $good){\Yii::trace('cartSumm = '.\Yii::$app->cart->cartSumm);
                 if(!empty($customer->cardNumber) && $good->discountSize == 0){
                     $good->setAttributes([
                         'discountSize'  =>  2,
@@ -323,10 +323,13 @@ class OrderForm extends Model{
                     'storeID'       =>  1
                 ]);
 
-                if($orderItem->save()){
-                    \Yii::$app->cart->remove($good->ID);
+                if($orderItem->save(false)){
+                    \Yii::$app->cart->remove($good->ID, false);
                 }
             }
+
+            \Yii::$app->cart->recalcCart();
+            \Yii::$app->cart->save();
 
             $customer->setAttributes([
                 'name'          =>  $this->customerName,
