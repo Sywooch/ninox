1b362e8b81de148725e3b209f9df4da68ee219fa
Hellios3000 <Skeptic.q@gmail.com>
2016-05-24 15:58:12 +0300

    fix banner4 migrate

--- console/migrations/m160524_125243_url_banner4.php
+++ console/migrations/m160524_125243_url_banner4.php
@@ -6,7 +6,7 @@ class m160524_125243_url_banner4 extends Migration
 {
     public function up()
     {
-        $banner = \common\models\bannerTranslation::findOne(['ID' => 489]);
+        $banner = \common\models\BannerTranslation::findOne(['ID' => 489]);
         if($banner){
             $banner->value = '{"goodID":33541,"image":"watch.png"}';
             $banner->save(false);
@@ -15,7 +15,7 @@ class m160524_125243_url_banner4 extends Migration
 
     public function down()
     {
-        $banner = \common\models\bannerTranslation::findOne(['ID' => 489]);
+        $banner = \common\models\BannerTranslation::findOne(['ID' => 489]);
         if($banner){
             $banner->value = '{"goodID":12816,"image":"watch.png"}';
             $banner->save(false);
