01f2581e3a3c39d2e52a445af1b27c544b7c700e
Hellios3000 <Skeptic.q@gmail.com>
2016-05-24 15:53:01 +0300

    banner on main

---
+++ console/migrations/m160524_125243_url_banner4.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+class m160524_122532_url_banner4 extends Migration
+{
+    public function up()
+    {
+        $banner = \common\models\bannerTranslationr::findOne(['ID' => 489]);
+        if($banner){
+            $banner->value = '{"goodID":33541,"image":"watch.png"}';
+            $banner->save(false);
+        }
+    }
+
+    public function down()
+    {
+        $banner = \common\models\bannerTranslationr::findOne(['ID' => 489]);
+        if($banner){
+            $banner->value = '{"goodID":12816,"image":"watch.png"}';
+            $banner->save(false);
+        }
+
+        echo "m160524_122532_url_banner4 cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
