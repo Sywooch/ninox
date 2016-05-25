8c1b07d042b13b73f13f73baaaff3f289ba703cb
Hellios3000 <Skeptic.q@gmail.com>
2016-05-25 18:12:40 +0300

    migration * замена картинок в меню

---
+++ console/migrations/m160525_150436_menu_img.php
@@ -0,0 +1,110 @@
+<?php
+
+use yii\db\Migration;
+
+class m160525_150436_menu_img extends Migration
+{
+    public function up()
+    {
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 156, 'order' => 1]);
+        if($catPhoto){
+            $catPhoto->photo = 'chasy.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 156, 'order' => 2]);
+        if($catPhoto){
+            $catPhoto->photo = 'tyolka.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 202, 'order' => 1]);
+        if($catPhoto){
+            $catPhoto->photo = 'shnur.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 202, 'order' => 2]);
+        if($catPhoto){
+            $catPhoto->photo = 'mozaika.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 367, 'order' => 1]);
+        if($catPhoto){
+            $catPhoto->photo = 'pudra.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 367, 'order' => 2]);
+        if($catPhoto){
+            $catPhoto->photo = 'rascheska.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 605, 'order' => 1]);
+        if($catPhoto){
+            $catPhoto->photo = 'podushka.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 605, 'order' => 2]);
+        if($catPhoto){
+            $catPhoto->photo = 'shariki.png';
+            $catPhoto->save(false);
+        }
+
+    }
+
+    public function down()
+    {
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 156, 'order' => 2]);
+        if($catPhoto){
+            $catPhoto->photo = 'chasy.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 156, 'order' => 1]);
+        if($catPhoto){
+            $catPhoto->photo = 'tyolka.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 202, 'order' => 2]);
+        if($catPhoto){
+            $catPhoto->photo = 'shnur.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 202, 'order' => 1]);
+        if($catPhoto){
+            $catPhoto->photo = 'mozaika.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 367, 'order' => 2]);
+        if($catPhoto){
+            $catPhoto->photo = 'pudra.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 367, 'order' => 1]);
+        if($catPhoto){
+            $catPhoto->photo = 'rascheska.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 605, 'order' => 2]);
+        if($catPhoto){
+            $catPhoto->photo = 'podushka.png';
+            $catPhoto->save(false);
+        }
+        $catPhoto = \common\models\CategoryPhoto::findOne(['categoryID' => 605, 'order' => 1]);
+        if($catPhoto){
+            $catPhoto->photo = 'shariki.png';
+            $catPhoto->save(false);
+        }
+
+        echo "m160525_150436_menu_img cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
