6400c3aefc7538e5e18fd01cd629d18069c8b082
Hellios3000 <Skeptic.q@gmail.com>
2016-06-01 18:40:19 +0300

    поправлено отображение картинок рекомендуемых товаров в акаунте/личные данные

--- frontend/modules/account/views/default/_review_items.php
+++ frontend/modules/account/views/default/_review_items.php
@@ -5,10 +5,16 @@
  * Date: 12/11/2015
  * Time: 12:29 PM
  */
+
+use yii\bootstrap\Html;
+
+$photo = \Yii::$app->params['cdn-link'].\Yii::$app->params['small-img-path'].$model->photo;
+
 ?>
 <div class="items">
     <div class="image">
-        <?=\yii\bootstrap\Html::img('https://krasota-style.com.ua/img/catalog/'.$model->photo)?>
+        <?=/*\yii\bootstrap\Html::img('https://krasota-style.com.ua/img/catalog/'.$model->photo).*/
+        Html::img($photo,[])?>
     </div>
     <div class="write-review">
         <?=\yii\bootstrap\Html::a(\Yii::t('shop', 'написать отзыв'), '/tovar/'.$model->good->link.'-g'.$model->itemID)?>
