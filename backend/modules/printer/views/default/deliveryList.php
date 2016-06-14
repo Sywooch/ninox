ed0567db9b2497d1f11a4852bf51fc475bfbc7e8
bobroid <n.gilko@gmail.com>
2016-06-14 19:13:55 +0300

    delivery list test

--- backend/modules/printer/views/default/deliveryList.php
+++ backend/modules/printer/views/default/deliveryList.php
@@ -1,4 +1,36 @@
 <?php
+use yii\bootstrap\Html;
+
 echo \kartik\grid\GridView::widget([
-    'dataProvider'  =>  $orders
+    'dataProvider'  =>  $orders,
+    'summary'       =>  false,
+    'columns'       =>  [
+        'number',
+        [
+            'attribute' =>  'customerName',
+            'value'     =>  function($model){
+                return $model->customerName.' '.$model->customerSurname;
+            }
+        ],
+        [
+            'attribute' =>  'customerPhone',
+            'value'     =>  function($model){
+                return \Yii::$app->formatter->asPhone($model->customerPhone);
+            }
+        ],
+        [
+            'attribute' =>  'deliveryRegion',
+            'format'    =>  'html',
+            'value'     =>  function($model){
+                return Html::tag('div', Html::tag('b', $model->deliveryCity)).Html::tag('div', Html::tag('small', $model->deliveryRegion));
+            }
+        ],
+        [
+            'attribute' =>  'paymentType',
+            'value'     =>  function($model){
+                return $model->paymentTypeModel->description;
+            }
+        ],
+        'actualAmount'
+    ]
 ]);
