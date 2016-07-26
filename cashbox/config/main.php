54ba012f3ab1c19a2c0c6ee0ff1a34ee8ac3468d
panchenkodv <lexx.klan@mail.ru>
2016-07-26 14:06:15 +0300

    shit! fall back!!!

--- cashbox/config/main.php
+++ cashbox/config/main.php
@@ -46,19 +46,5 @@ return [
         ],
         'urlManager' => require(__DIR__.'/urlManager.php'),
     ],
-    'modules'   =>  [
-        'gridview' =>  [
-            'class' => '\kartik\grid\Module'
-        ],
-        'debug' => [
-            'class' => 'yii\debug\Module',
-            'allowedIPs' => ['80.78.45.114', '127.0.0.1', '::1'],
-            'panels' => [
-                'views' => ['class' => 'common\panels\ViewsPanel'],
-                'version' => ['class' => 'common\panels\VersionPanel'],
-                'userIP' => ['class' => 'common\panels\UserIPPanel'],
-            ],
-        ],
-    ],
     'params' => $params,
 ];
