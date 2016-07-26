295b7f5171828cdcf345cc8d787acfe03a039b48
panchenkodv <lexx.klan@mail.ru>
2016-07-26 14:01:27 +0300

    turn on debug panel in backend and cashbox

--- cashbox/config/params.php
+++ cashbox/config/params.php
@@ -5,7 +5,7 @@ return [
         '127.0.0.1', '80.78.45.114', '37.52.216.195', '95.111.137.138'
     ],
     'frontend'  =>  'http://krasota-style.com.ua',
-    'backend'   =>  'http://admin.krasota-style.com.ua',
+    'backend'   =>  'http://adm.krasota-style.com.ua',
     'autologin' =>  [],
     'allowedUsers' =>  [],
     'configuration' =>  [],
