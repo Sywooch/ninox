c5d5ce04c4c861fd51ea422bf4a654286926b69f
panchenkodv <lexx.klan@mail.ru>
2016-08-02 10:05:59 +0300

    try fix autologin

--- common/components/Request.php
+++ common/components/Request.php
@@ -12,7 +12,9 @@ namespace common\components;
 class Request extends \yii\web\Request{
 
     public function getUserIP(){
-        return empty($_SERVER["HTTP_CF_CONNECTING_IP"]) ? empty($_SERVER['HTTP_X_REAL_IP']) ? parent::getUserIP() : $_SERVER['HTTP_X_REAL_IP'] : $_SERVER["HTTP_CF_CONNECTING_IP"];
+        return empty($_SERVER["HTTP_CF_CONNECTING_IP"]) ?
+            (empty($_SERVER['HTTP_X_REAL_IP']) ? parent::getUserIP() : $_SERVER['HTTP_X_REAL_IP']) :
+            $_SERVER["HTTP_CF_CONNECTING_IP"];
     }
 
 }
